617856a4312ccea930048d692da9966e3b16a3a6
Tim Schofield <timothyschofield@hotmail.com>
2023-11-08 18:45:05 +0000

    Little demo using GPT-4

--- OCR_GPT.py
+++ OCR_GPT.py
@@ -21,23 +21,32 @@ WARNING: You have to quit out of VS Code (this IDE) and go back in before the ne
 FYI: To see all system environment variables,
         go to the DOS COMMAND PROMPT and type "set"
 
+-----------------------------------------------------------------------------------   
+https://blog.roboflow.com/gpt-4-image-classification/   
+On November 6th, 2023, OpenAI released a vision-enabled version of the GPT-4 API. This API, referred to by the gpt-4-vision-preview identifier, enables you to ask a question and provide an image as context. We previously reported on GPT 4V's capabilities, noting impressive performance in image understanding. These capabilities are perfect for classification.
+
+
 """
 import os
 # This code is for v1 of the openai package: pypi.org/project/openai
 # pip install openai
 from openai import OpenAI
 
-api_key = os.environ["OPENAI_API_KEY"]
+my_api_key = os.environ["OPENAI_API_KEY"]
 
-client = OpenAI(api_key=api_key)   # openai version 1.1.1
+client = OpenAI(api_key=my_api_key)   # openai version 1.1.1
 
 try:
 
+    # "gpt-3.5-turbo", currently points to gpt-3.5-turbo-0613. Will point to gpt-3.5-turbo-1106 starting Dec 11, 2023. 
+    # post https://api.openai.com/v1/chat/completions
     completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
+    model="gpt-4",
+
+    # Grammar correction
     messages=[
-        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+        {"role": "system", "content": "You will be provided with statements, and your task is to convert them to standard English."},
+        {"role": "user", "content": "She no went to the market."}
     ]
     )
 
@@ -53,6 +62,10 @@ Error code: 429 - {'error': {'message': 'You exceeded your current quota, please
 
 You can view the rate and usage limits for your organization under the limits section of your account settings.                                                    
 OpenAI Icon > Settings > Limits
+08 Nov 2023 - put my credit limit up to $10.00 (credit card details and everything)
+DF84C4A2-0001	Paid  $12.00	8 Nov 2023, 08:34	-- Yay, it works
+ChatCompletionMessage(content='She did not go to the market.', role='assistant', function_call=None, tool_calls=None)
+
 
 If you want to use the new GPT-4 AI model, you'll just need to subscribe to ChatGPT Plus. 
 For developers, you can access the GPT-4 API. 
