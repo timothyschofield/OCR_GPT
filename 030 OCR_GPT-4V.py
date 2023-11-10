bf6e51505163a7dfb5124d49f84ea90cdef93cb1
Tim Schofield <timothyschofield@hotmail.com>
2023-11-10 17:14:58 +0000

    Batch Processing

--- 030 OCR_GPT-4V.py
+++ 030 OCR_GPT-4V.py
@@ -40,6 +40,7 @@ OpenAI Icon > Settings > Limits
 
 """
 import os
+import time
 # This code is for v1 of the openai package: pypi.org/project/openai
 # pip install openai
 from openai import OpenAI
@@ -53,94 +54,85 @@ try:
     # https://platform.openai.com/docs/guides/vision
     # https://help.openai.com/en/articles/8555496-gpt-v-api
 
-    request = "Please OCR this hebarium lable and extract collector and collector number, date, family, genus, species, altitude, latitude, longitude, location, country, description, language and the barcode number which begins with the letter 'K'"
-    # test_url = "https://d2seqvvyy3b8p2.cloudfront.net/d1f65c385d649f6770035348dd05c7ee.jpg" # shit handwritten
     
-    test_url = "https://d2seqvvyy3b8p2.cloudfront.net/2ca62a26221a397d6942874b6ee7a225.jpg" # better handwritting
-
+    # test_url = "https://d2seqvvyy3b8p2.cloudfront.net/2ca62a26221a397d6942874b6ee7a225.jpg" # better handwritting
+    
+    # test_url = "https://d2seqvvyy3b8p2.cloudfront.net/d1f65c385d649f6770035348dd05c7ee.jpg" # shit handwritten
     #test_url = "https://d2seqvvyy3b8p2.cloudfront.net/17ab52f8bf423934e72f326506e26850.jpg" # printed
     
-
-   
-    
-
     #request = "Please OCR this old handwriting"
     #test_url = "https://c7.alamy.com/comp/MF9K2X/facsimile-of-a-portion-of-the-letter-written-by-oliver-cromwell-to-william-lenthall-speaker-of-the-house-of-commons-announcing-the-victory-at-the-battle-of-naseby-in-1645-from-old-england-a-pictorial-museum-published-1847-MF9K2X.jpg"
     
+    request = "Please OCR this hebarium lable and extract collector and collector number, date, family, genus, species, altitude, latitude, longitude, location, country, description, language and the barcode number which begins with the letter 'K'"
+    """
+    url_list = ["https://d2seqvvyy3b8p2.cloudfront.net/650cef96509839e53586e9cb770ce2f3.jpg",
+    "https://d2seqvvyy3b8p2.cloudfront.net/f76836bbb9d6f938e47d1008d8f09dd2.jpg",
+    "https://d2seqvvyy3b8p2.cloudfront.net/368a769e34246aca7a392b5c20efbd78.jpg",
+    "https://d2seqvvyy3b8p2.cloudfront.net/7a90cfd7e9a4e3c621c32d32d9514c96.jpg",
+    "https://d2seqvvyy3b8p2.cloudfront.net/a3ee08998385e8402b8aa4c5b9b9f613.jpg",
+    "https://d2seqvvyy3b8p2.cloudfront.net/bb7be423df727cd8a79dad1a4fce9640.jpg",
+    "https://d2seqvvyy3b8p2.cloudfront.net/7f1f4ee4d91b3713a59ab993aa7eee13.jpg",
+    "https://d2seqvvyy3b8p2.cloudfront.net/871bf83bb330c2cd2b9b114d6590f89d.jpg",
+    "https://d2seqvvyy3b8p2.cloudfront.net/bbeee4e27aa37443638383ad7b14d016.jpg",
+    "https://d2seqvvyy3b8p2.cloudfront.net/6b947a6fa40888bd7cd4882973715db6.jpg",
+    "https://d2seqvvyy3b8p2.cloudfront.net/26a1f3f5985e1f11421528e4715bb466.jpg"]
+    """
+    url_list = ["https://d2seqvvyy3b8p2.cloudfront.net/2ca62a26221a397d6942874b6ee7a225.jpg"]
+
+    print("========================== START OUTPUT =============================")
+
+    for test_url in url_list:
+
+        print(test_url)
+       
+        ocr_output = client.chat.completions.create(
+            model="gpt-4-vision-preview",
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": request},
+                        {
+                            "type": "image_url",
+                            "image_url": test_url
+                        }
+                    ]
+                }  
+            ],
+            max_tokens=500,  
+        )
+        
+        # Works
+        json_input = ocr_output.choices[0].message.content
+        print(json_input)
     
-    ocr_output = client.chat.completions.create(
-        model="gpt-4-vision-preview",
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                    {"type": "text", "text": request},
-                    {
-                        "type": "image_url",
-                        "image_url": test_url
-                    }
+        json_output = client.chat.completions.create(
+            model="gpt-4", 
+            # Grammar correction
+            messages=[
+                {"role": "system", "content": "First, delete all occurances of '\n  '. Format this as JSON where 'Collector', 'Collector number', 'Date', 'Family', 'Genus', 'Species','Altitude', 'Location', 'Latitude', 'Longitude', 'Country', 'Description' and 'Barcode number' are keys"},
+                {"role": "user", "content": str(json_input)}
                 ]
-            }  
-        ],
-        max_tokens=500,  
-    )
-    
-    # Works
-    print(ocr_output.choices[0].message)
-   
-    json_output = client.chat.completions.create(
-        model="gpt-4", 
-        # Grammar correction
-        messages=[
-            {"role": "system", "content": "First, delete all occurances of '\n  '. Format this as JSON where 'Collector', 'Collector number', Date, 'Family', 'Genus', 'Species','Altitude', 'Location', 'Latitude', 'Longitude', 'Country', 'Description' and 'Barcode number' are keys"},
-            {"role": "user", "content": str(ocr_output.choices[0].message)}
-            ]
-    )
+        )
 
-    print(json_output.choices[0].message.content)
+        sql_input = json_output.choices[0].message.content
 
-    """
-    {
-    "Collector": "P.S. Lavarack",
-    "Collector number": "PSL4001",
-    "Altitude": "2 m.",
-    "Location": "12km S of Cardwell, W of highway near Cleghorn",
-    "Latitude": "18°23'S",
-    "Longitude": "146°05'E",
-    "Country": "Queensland, Australia",
-    "Description": "Herbaceous viviform woodland, poorly drained. Male to about 40cm tall.",
-    "Barcode number": "K000742667"
-    }
-    """
+        print(sql_input)
+        sql_output = client.chat.completions.create(
+            model="gpt-4", 
+            # Grammar correction
+            messages=[
+                {"role": "system", "content": "This input is JSON. Format the input as an SQL INSERT statement for a table named 'specimenCards' where the keys are the column names and the values are values"},
+                {"role": "user", "content": str(sql_input)}
+                ]
+        )
 
-    sql_output = client.chat.completions.create(
-        model="gpt-4", 
-        # Grammar correction
-        messages=[
-            {"role": "system", "content": "This input is JSON. Format the input as an SQL INSERT statement for a table named 'specimenCards' where the keys are the column names and the values are values"},
-            {"role": "user", "content": str(json_output.choices[0].message.content)}
-            ]
-    )
+        print(sql_output.choices[0].message.content)
 
-    print(sql_output.choices[0].message.content)
+        time.sleep(5)
 
-    """
-    {
-    "Collector": "P.S. Lavarack",
-    "Collector number": "PSL4001",
-    "Altitude": "2 m.",
-    "Location": "12km S of Cardwell, W of highway near Cleghorn",
-    "Latitude": "18°23'S",
-    "Longitude": "146°05'E",
-    "Country": "Queensland, Australia",
-    "Description": "Herbaceous viviform woodland, poorly drained. Male to about 40cm tall.",
-    "Barcode number": "K000742667"
-    }
+    print("========================== STOP OUTPUT =============================")   
 
-    INSERT INTO specimenCards (Collector, Collector number, Altitude, Location, Latitude, Longitude, Country, Description, Barcode number) 
-    VALUES ('P.S. Lavarack', 'PSL4001', '2 m.', '12km S of Cardwell, W of highway near Cleghorn', '18°23''S', '146°05''E', 'Queensland, Australia', 'Herbaceous viviform woodland, poorly drained. Male to about 40cm tall.', 'K000742667');
-
-    """
 except Exception as ex:
     print("Exception:", ex)
 
